891a1b47383ef293aa80ce313ebce9cad00ab37b
Joanna Dinh <jndinh13@gmail.com>
2022-03-10 13:57:58 -0500

    sign_in with :bypass option is deprecated, switch to using bypass_sign_in

--- app/controllers/devise/displayqr_controller.rb
+++ app/controllers/devise/displayqr_controller.rb
@@ -23,7 +23,7 @@ class Devise::DisplayqrController < DeviseController
 
     if resource.set_gauth_enabled(params[resource_name]['gauth_enabled'])
       set_flash_message :notice, (resource.gauth_enabled? ? :enabled : :disabled)
-      sign_in scope, resource, :bypass => true
+      bypass_sign_in resource, scope: scope
       redirect_to stored_location_for(scope) || :root
     else
       render :show
@@ -35,7 +35,7 @@ class Devise::DisplayqrController < DeviseController
       resource.send(:assign_auth_secret)
       resource.save
       set_flash_message :notice, :newtoken
-      sign_in scope, resource, :bypass => true
+      bypass_sign_in resource, scope: scope
       redirect_to [resource_name, :displayqr]
     else
       redirect_to :root
